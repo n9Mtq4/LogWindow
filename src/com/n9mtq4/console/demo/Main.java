333eb7f5539479c7464c486c17fdb3e18f74e689
n9Mtq4 <bill.wresnahan@gmail.com>
2014-10-20 17:00:49 -0400

    Demo example

---
+++ src/com/n9mtq4/console/demo/Main.java
@@ -0,0 +1,32 @@
+/*
+ * NOTE: This is added by intellij IDE. Disregard this message if there is another copyright later in the file.
+ * Copyright (C) 2014  Will (n9Mtq4) Bresnahan
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.n9mtq4.console.demo;
+
+import com.n9mtq4.console.lib.Console;
+
+/**
+ * Created by Will on 10/20/14.
+ */
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		MyParser myParser = new MyParser();
+		new Console(myParser);
+		
+	}
+	
+}
