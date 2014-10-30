9028d1a706c1387684eacf127e8b7568889c0bbb
n9Mtq4 <bill.wresnahan@gmail.com>
2014-10-30 18:12:18 -0400

    Manager to redirect standard out

---
+++ src/com/n9mtq4/console/lib/managers/StdoutRedirect.java
@@ -0,0 +1,96 @@
+package com.n9mtq4.console.lib.managers;
+
+import com.n9mtq4.console.lib.Console;
+
+import java.io.PrintStream;
+import java.text.MessageFormat;
+
+/**
+ * Created by Will on 10/29/14.
+ */
+public class StdoutRedirect extends PrintStream {
+	
+	private Console c;
+	private PrintStream backup;
+	private boolean showLocation;
+	private boolean on;
+	
+	public StdoutRedirect(Console c) {
+		super(System.out);
+		this.c = c;
+	}
+	
+	public void turnOn(boolean showLocation) {
+		
+		this.on = true;
+		this.showLocation = showLocation;
+		this.backup = System.out;
+		System.setOut(this);
+		
+	}
+	
+	public void turnOff() {
+		
+		this.on = false;
+		System.setOut(backup);
+		
+	}
+	
+	@Override
+	public void print(String s) {
+		if (showLocation) {
+			c.print(getLocation() + s);
+		}else {
+			c.print(s);
+		}
+	}
+	
+	@Override
+	public void print(Object o) {
+		if (showLocation) {
+			c.print(getLocation() + o.toString());
+		}else {
+			c.print(o.toString());
+		}
+	}
+	
+	@Override
+	public void println(Object x) {
+		if (showLocation) {
+			c.println(getLocation() + x.toString());
+		}else {
+			c.println(x.toString());
+		}
+	}
+	
+	@Override
+	public void println(String x) {
+		if (showLocation) {
+			c.println(getLocation() + x);
+		}else {
+			c.println(x);
+		}
+	}
+	
+	private String getLocation() {
+		StackTraceElement element = Thread.currentThread().getStackTrace()[3];
+		return MessageFormat.format("({0}:{1, number,#}): ", element.getFileName(), element.getLineNumber());
+	}
+	
+	public Console getC() {
+		return c;
+	}
+	
+	public PrintStream getBackup() {
+		return backup;
+	}
+	
+	public boolean isShowLocation() {
+		return showLocation;
+	}
+	
+	public boolean isOn() {
+		return on;
+	}
+	
+}
