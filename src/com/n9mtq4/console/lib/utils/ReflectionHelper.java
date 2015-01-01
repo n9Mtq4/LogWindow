791714b380d33a69569aba34373d5c572debf2e9
n9Mtq4 <bill.wresnahan@gmail.com>
2015-01-01 14:32:04 -0500

    major change to reflection

--- src/com/n9mtq4/console/lib/utils/ReflectionHelper.java
+++ src/com/n9mtq4/console/lib/utils/ReflectionHelper.java
@@ -17,6 +17,7 @@ package com.n9mtq4.console.lib.utils;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 /**
@@ -48,6 +49,14 @@ public class ReflectionHelper {
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static int getInt(String fieldName, Object obj) {
+		return getInt(fieldName, obj, obj.getClass());
+	}
+	
+	public static void setInt(int x, String fieldName, Object obj) {
+		setInt(x, fieldName, obj, obj.getClass());
 	}
 	
 	public static int getStaticInt(String fieldName, Class clazz) {
@@ -92,6 +101,14 @@ public class ReflectionHelper {
 		}
 	}
 	
+	public static byte getByte(String fieldName, Object obj) {
+		return getByte(fieldName, obj, obj.getClass());
+	}
+	
+	public static void setByte(byte x, String fieldName, Object obj) {
+		setByte(x, fieldName, obj, obj.getClass());
+	}
+	
 	public static byte getStaticByte(String fieldName, Class clazz) {
 		try {
 			Field f = clazz.getDeclaredField(fieldName);
@@ -132,6 +149,14 @@ public class ReflectionHelper {
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static boolean getBoolean(String fieldName, Object obj) {
+		return getBoolean(fieldName, obj, obj.getClass());
+	}
+	
+	public static void setBoolean(boolean x, String fieldName, Object obj) {
+		setBoolean(x, fieldName, obj, obj.getClass());
 	}
 	
 	public static boolean getStaticBoolean(String fieldName, Class clazz) {
@@ -176,6 +201,14 @@ public class ReflectionHelper {
 		}
 	}
 	
+	public static char getChar(String fieldName, Object obj) {
+		return getChar(fieldName, obj, obj.getClass());
+	}
+	
+	public static void setChar(char x, String fieldName, Object obj) {
+		setChar(x, fieldName, obj, obj.getClass());
+	}
+	
 	public static char getStaticChar(String fieldName, Class clazz) {
 		try {
 			Field f = clazz.getDeclaredField(fieldName);
@@ -216,6 +249,14 @@ public class ReflectionHelper {
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static float getFloat(String fieldName, Object obj) {
+		return getFloat(fieldName, obj, obj.getClass());
+	}
+	
+	public static void setFloat(float x, String fieldName, Object obj) {
+		setFloat(x, fieldName, obj, obj.getClass());
 	}
 	
 	public static float getStaticFloat(String fieldName, Class clazz) {
@@ -260,6 +301,14 @@ public class ReflectionHelper {
 		}
 	}
 	
+	public static double getDouble(String fieldName, Object obj) {
+		return getDouble(fieldName, obj, obj.getClass());
+	}
+	
+	public static void setDouble(double x, String fieldName, Object obj) {
+		setDouble(x, fieldName, obj, obj.getClass());
+	}
+	
 	public static double getStaticDouble(String fieldName, Class clazz) {
 		try {
 			Field f = clazz.getDeclaredField(fieldName);
@@ -300,6 +349,14 @@ public class ReflectionHelper {
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static long getLong(String fieldName, Object obj) {
+		return getLong(fieldName, obj, obj.getClass());
+	}
+	
+	public static void setLong(long x, String fieldName, Object obj) {
+		setLong(x, fieldName, obj, obj.getClass());
 	}
 	
 	public static long getStaticLong(String fieldName, Class clazz) {
@@ -344,6 +401,14 @@ public class ReflectionHelper {
 		}
 	}
 	
+	public static Object getObject(String fieldName, Object obj) {
+		return getObject(fieldName, obj, obj.getClass());
+	}
+	
+	public static void setObject(Object x, String fieldName, Object obj) {
+		setObject(x, fieldName, obj, obj.getClass());
+	}
+	
 	public static Object getStaticObject(String fieldName, Class clazz) {
 		try {
 			Field f = clazz.getDeclaredField(fieldName);
@@ -365,12 +430,8 @@ public class ReflectionHelper {
 		}
 	}
 	
-	public static Object callConstructor(Class clazz, Object[] params) {
+	public static Object callConstructor(Class clazz, Class[] classParams, Object[] params) {
 		try {
-			Class[] classParams = new Class[params.length];
-			for (int i = 0; i < classParams.length; i++) {
-				classParams[i] = params[i].getClass();
-			}
 			Constructor c = clazz.getDeclaredConstructor(classParams);
 			c.setAccessible(true);
 			return c.newInstance(params);
@@ -380,128 +441,172 @@ public class ReflectionHelper {
 		}
 	}
 	
-	public static Object callObjectMethod(String methodName, Object obj, Class clazz, Object[] params) {
+	public static Object callConstructor(Class clazz, Object[] params) {
+		return callConstructor(clazz, getClassParams(params), params);
+	}
+	
+	public static Object callObjectMethod(String methodName, Object obj, Class clazz, Class[] classParams, Object[] params) {
+		Method m = null;
 		try {
-			Class[] classParams = new Class[params.length];
-			for (int i = 0; i < classParams.length; i++) {
-				classParams[i] = params[i].getClass();
-			}
-			Method m = clazz.getDeclaredMethod(methodName, classParams);
+			m = clazz.getDeclaredMethod(methodName, classParams);
 			m.setAccessible(true);
 			return m.invoke(obj, params);
-		}catch (Exception e) {
+		}catch (NoSuchMethodException e) {
 			e.printStackTrace();
-			return null;
+		}catch (InvocationTargetException e) {
+			e.printStackTrace();
+		}catch (IllegalAccessException e) {
+			e.printStackTrace();
 		}
+		return null;
+	}
+	
+	public static Object callObjectMethod(String methodName, Object obj, Class clazz, Object[] params) {
+		return callObjectMethod(methodName, obj, clazz, getClassParams(params), params);
+	}
+	
+	public static Object callStaticObjectMethod(String methodName, Class clazz, Class[] classParams, Object[] params) {
+		return callObjectMethod(methodName, null, clazz, classParams, params);
+	}
+	
+	public static Object callStaticObjectMethod(String methodName, Class clazz, Object[] params) {
+		return callStaticObjectMethod(methodName, clazz, getClassParams(params), params);
 	}
 	
 	public static void callVoidMethod(String methodName, Object obj, Class clazz, Object[] params) {
-		callObjectMethod(methodName, obj, clazz, params);
+		callVoidMethod(methodName, obj, clazz, getClassParams(params), params);
 	}
 	
-	public static Integer callIntMethod(String methodName, Object obj, Class clazz, Object[] params) {
-		try {
-			Class[] classParams = new Class[params.length];
-			for (int i = 0; i < classParams.length; i++) {
-				classParams[i] = params[i].getClass();
-			}
-			Method m = clazz.getDeclaredMethod(methodName, classParams);
-			m.setAccessible(true);
-			return (Integer) m.invoke(obj, params);
-		}catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
+	public static void callVoidMethod(String methodName, Object obj, Class clazz, Class[] classParams, Object[] params) {
+		callObjectMethod(methodName, obj, clazz, classParams, params);
 	}
 	
-	public static Byte callByteMethod(String methodName, Object obj, Class clazz, Object[] params) {
-		try {
-			Class[] classParams = new Class[params.length];
-			for (int i = 0; i < classParams.length; i++) {
-				classParams[i] = params[i].getClass();
-			}
-			Method m = clazz.getDeclaredMethod(methodName, classParams);
-			m.setAccessible(true);
-			return (Byte) m.invoke(obj, params);
-		}catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
+	public static void callStaticVoidMethod(String methodName, Class clazz, Object[] params) {
+		callStaticVoidMethod(methodName, clazz, getClassParams(params), params);
 	}
 	
-	public static Boolean callBooleanMethod(String methodName, Object obj, Class clazz, Object[] params) {
-		try {
-			Class[] classParams = new Class[params.length];
-			for (int i = 0; i < classParams.length; i++) {
-				classParams[i] = params[i].getClass();
-			}
-			Method m = clazz.getDeclaredMethod(methodName, classParams);
-			m.setAccessible(true);
-			return (Boolean) m.invoke(obj, params);
-		}catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
+	public static void callStaticVoidMethod(String methodName, Class clazz, Class[] classParams, Object[] params) {
+		callStaticObjectMethod(methodName, clazz, classParams, params);
 	}
 	
-	public static Character callCharMethod(String methodName, Object obj, Class clazz, Object[] params) {
-		try {
-			Class[] classParams = new Class[params.length];
-			for (int i = 0; i < classParams.length; i++) {
-				classParams[i] = params[i].getClass();
-			}
-			Method m = clazz.getDeclaredMethod(methodName, classParams);
-			m.setAccessible(true);
-			return (Character) m.invoke(obj, params);
-		}catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
+	public static int callIntMethod(String methodName, Object obj, Class clazz, Class[] classParams, Object[] params) {
+		return (Integer) callObjectMethod(methodName, obj, clazz, classParams, params);
 	}
 	
-	public static Float callFloatMethod(String methodName, Object obj, Class clazz, Object[] params) {
-		try {
-			Class[] classParams = new Class[params.length];
-			for (int i = 0; i < classParams.length; i++) {
-				classParams[i] = params[i].getClass();
-			}
-			Method m = clazz.getDeclaredMethod(methodName, classParams);
-			m.setAccessible(true);
-			return (Float) m.invoke(obj, params);
-		}catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
+	public static int callIntMethod(String methodName, Object obj, Class clazz, Object[] params) {
+		return callIntMethod(methodName, obj, clazz, getClassParams(params), params);
 	}
 	
-	public static Double callDoubleMethod(String methodName, Object obj, Class clazz, Object[] params) {
-		try {
-			Class[] classParams = new Class[params.length];
-			for (int i = 0; i < classParams.length; i++) {
-				classParams[i] = params[i].getClass();
-			}
-			Method m = clazz.getDeclaredMethod(methodName, classParams);
-			m.setAccessible(true);
-			return (Double) m.invoke(obj, params);
-		}catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
+	public static int callStaticIntMethod(String methodName, Class clazz, Class[] classParams, Object[] params) {
+		return (Integer) callStaticObjectMethod(methodName, clazz, classParams, params);
 	}
 	
-	public static Long callLongMethod(String methodName, Object obj, Class clazz, Object[] params) {
-		try {
-			Class[] classParams = new Class[params.length];
-			for (int i = 0; i < classParams.length; i++) {
-				classParams[i] = params[i].getClass();
-			}
-			Method m = clazz.getDeclaredMethod(methodName, classParams);
-			m.setAccessible(true);
-			return (Long) m.invoke(obj, params);
-		}catch (Exception e) {
-			e.printStackTrace();
-			return null;
+	public static int callStaticIntMethod(String methodName, Class clazz, Object[] params) {
+		return callStaticIntMethod(methodName, clazz, getClassParams(params), params);
+	}
+	
+	public static byte callByteMethod(String methodName, Object obj, Class clazz, Class[] classParams, Object[] params) {
+		return (Byte) callObjectMethod(methodName, obj, clazz, classParams, params);
+	}
+	
+	public static byte callByteMethod(String methodName, Object obj, Class clazz, Object[] params) {
+		return callByteMethod(methodName, obj, clazz, getClassParams(params), params);
+	}
+	
+	public static byte callStaticByteMethod(String methodName, Class clazz, Class[] classParams, Object[] params) {
+		return (Byte) callStaticObjectMethod(methodName, clazz, classParams, params);
+	}
+	
+	public static byte callStaticByteMethod(String methodName, Class clazz, Object[] params) {
+		return callStaticByteMethod(methodName, clazz, getClassParams(params), params);
+	}
+	
+	public static boolean callBooleanMethod(String methodName, Object obj, Class clazz, Class[] classParams, Object[] params) {
+		return (Boolean) callObjectMethod(methodName, obj, clazz, classParams, params);
+	}
+	
+	public static boolean callBooleanMethod(String methodName, Object obj, Class clazz, Object[] params) {
+		return callBooleanMethod(methodName, obj, clazz, getClassParams(params), params);
+	}
+	
+	public static boolean callStaticBooleanMethod(String methodName, Class clazz, Class[] classParams, Object[] params) {
+		return (Boolean) callStaticObjectMethod(methodName, clazz, classParams, params);
+	}
+	
+	public static boolean callStaticBooleanMethod(String methodName, Class clazz, Object[] params) {
+		return callStaticBooleanMethod(methodName, clazz, getClassParams(params), params);
+	}
+	
+	public static char callCharMethod(String methodName, Object obj, Class clazz, Class[] classParams, Object[] params) {
+		return (Character) callObjectMethod(methodName, obj, clazz, classParams, params);
+	}
+	
+	public static char callCharMethod(String methodName, Object obj, Class clazz, Object[] params) {
+		return callCharMethod(methodName, obj, clazz, getClassParams(params), params);
+	}
+	
+	public static char callStaticCharMethod(String methodName, Class clazz, Class[] classParams, Object[] params) {
+		return (Character) callStaticObjectMethod(methodName, clazz, classParams, params);
+	}
+	
+	public static char callStaticCharMethod(String methodName, Class clazz, Object[] params) {
+		return callStaticCharMethod(methodName, clazz, getClassParams(params), params);
+	}
+	
+	public static float callFloatMethod(String methodName, Object obj, Class clazz, Class[] classParams, Object[] params) {
+		return (Float) callObjectMethod(methodName, obj, clazz, classParams, params);
+	}
+	
+	public static float callFloatMethod(String methodName, Object obj, Class clazz, Object[] params) {
+		return callFloatMethod(methodName, obj, clazz, getClassParams(params), params);
+	}
+	
+	public static float callStaticFloatMethod(String methodName, Class clazz, Class[] classParams, Object[] params) {
+		return (Float) callStaticObjectMethod(methodName, clazz, classParams, params);
+	}
+	
+	public static float callStaticFloatMethod(String methodName, Class clazz, Object[] params) {
+		return callStaticFloatMethod(methodName, clazz, getClassParams(params), params);
+	}
+	
+	public static double callDoubleMethod(String methodName, Object obj, Class clazz, Class[] classParams, Object[] params) {
+		return (Double) callObjectMethod(methodName, obj, clazz, classParams, params);
+	}
+	
+	public static double callDoubleMethod(String methodName, Object obj, Class clazz, Object[] params) {
+		return callDoubleMethod(methodName, obj, clazz, getClassParams(params), params);
+	}
+	
+	public static double callStaticDoubleMethod(String methodName, Class clazz, Class[] classParams, Object[] params) {
+		return (Double) callStaticObjectMethod(methodName, clazz, classParams, params);
+	}
+	
+	public static double callStaticDoubleMethod(String methodName, Class clazz, Object[] params) {
+		return callStaticDoubleMethod(methodName, clazz, getClassParams(params), params);
+	}
+	
+	public static long callLongMethod(String methodName, Object obj, Class clazz, Class[] classParams, Object[] params) {
+		return (Long) callObjectMethod(methodName, obj, clazz, classParams, params);
+	}
+	
+	public static long callLongMethod(String methodName, Object obj, Class clazz, Object[] params) {
+		return callLongMethod(methodName, obj, clazz, getClassParams(params), params);
+	}
+	
+	public static long callStaticLongMethod(String methodName, Class clazz, Class[] classParams, Object[] params) {
+		return (Long) callStaticObjectMethod(methodName, clazz, classParams, params);
+	}
+	
+	public static long callStaticLongMethod(String methodName, Class clazz, Object[] params) {
+		return callStaticLongMethod(methodName, clazz, getClassParams(params), params);
+	}
+	
+	private static Class[] getClassParams(Object[] params) {
+		Class[] classParams = new Class[params.length];
+		for (int i = 0; i < classParams.length; i++) {
+			classParams[i] = params[i].getClass();
 		}
+		return classParams;
 	}
 	
 }
