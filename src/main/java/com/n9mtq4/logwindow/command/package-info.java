9816cadb1de1bccb492073949d0d5e00c38b2e13
n9Mtq4 <bill.wresnahan@gmail.com>
2015-09-11 16:43:59 -0400

    package info

---
+++ src/main/java/com/n9mtq4/logwindow/command/package-info.java
@@ -0,0 +1,22 @@
+/*
+ * NOTE: This is added by intellij IDE. Disregard this copyright if there is another copyright later in the file.
+ * Copyright (C) 2015  Will (n9Mtq4) Bresnahan
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * This package contains the {@link com.n9mtq4.logwindow.command.ConsoleCommand}.
+ * This class contains some String parsing utils.
+ *
+ * @author Will "n9Mtq4" Bresnahan
+ */
+package com.n9mtq4.logwindow.command;
